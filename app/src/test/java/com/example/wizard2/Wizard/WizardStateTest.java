196e3e7f11541091d33655f96ddc70ac84700180
Keisha C <chingk20@up.edu>
2019-11-22 17:30:07 -0800

    wrote some tests for WizardState

--- app/src/test/java/com/example/wizard2/Wizard/WizardStateTest.java
+++ app/src/test/java/com/example/wizard2/Wizard/WizardStateTest.java
@@ -16,6 +16,72 @@ public class WizardStateTest {
 
     }
 
+
+    @Test
+    public void calculateWinner(){
+        //Keisha wrote this test
+        WizardState testState = new WizardState();
+        for(int i=0; i<4; i++) {
+            testState.setPlayerBidsWon(0, i);
+        }
+        WizardCards card1 = new WizardCards("heart", 2);
+        testState.cardsPlayed.set(0, card1);
+        WizardCards card2 = new WizardCards("diamond", 7);
+        testState.cardsPlayed.set(1, card2);
+        WizardCards card3 = new WizardCards("spade", 3);
+        testState.cardsPlayed.set(2, card3);
+        WizardCards card4 = new WizardCards("club", 14);
+        testState.cardsPlayed.set(3, card4);
+        testState.setTrumpSuit("spade");
+        testState.calculateWinner();
+        assertEquals(0, (int)testState.getPlayerBidsWon().get(0));
+        assertEquals(0, (int)testState.getPlayerBidsWon().get(1));
+        assertEquals(1, (int)testState.getPlayerBidsWon().get(2));
+        assertEquals(0, (int)testState.getPlayerBidsWon().get(3));
+    }
+
+    @Test
+    public void resetImage(){
+        //Keisha wrote this test
+        WizardState testState = new WizardState();
+        WizardCards card = new WizardCards("heart", 2);
+        for(int i=0; i<4; i++){
+        testState.cardsPlayed.set(i, card);
+        }
+        assertEquals(card, testState.getCardsPlayed().get(0));
+        assertEquals(card, testState.getCardsPlayed().get(1));
+        assertEquals(card, testState.getCardsPlayed().get(2));
+        assertEquals(card, testState.getCardsPlayed().get(3));
+        testState.resetImage();
+        assertEquals(null, testState.getCardsPlayed().get(0));
+        assertEquals(null, testState.getCardsPlayed().get(1));
+        assertEquals(null, testState.getCardsPlayed().get(2));
+        assertEquals(null, testState.getCardsPlayed().get(3));
+    }
+
+    @Test
+    public void calculateScores(){
+        //Keisha wrote this test
+        WizardState testState = new WizardState();
+        testState.player0.setBidNum(2);
+        testState.player1.setBidNum(4);
+        testState.player2.setBidNum(1);
+        testState.player3.setBidNum(2);
+        testState.setPlayerBidsWon(7, 0);
+        testState.setPlayerBidsWon(6, 1);
+        testState.setPlayerBidsWon(1, 2);
+        testState.setPlayerBidsWon(2, 3);
+        testState.player0.setPlayerScore(0);
+        testState.player1.setPlayerScore(0);
+        testState.player2.setPlayerScore(0);
+        testState.player3.setPlayerScore(0);
+        testState.calculateScores();
+        assertEquals(-50,testState.player0.getPlayerScore());
+        assertEquals(-20,testState.player1.getPlayerScore());
+        assertEquals(30,testState.player2.getPlayerScore());
+        assertEquals(40,testState.player3.getPlayerScore());
+    }
+
     @Test
     public void getPlayerTurn() {
         //Kori wrote this test
@@ -34,8 +100,6 @@ public class WizardStateTest {
         assertTrue(testState.getGameStage() == 13);
         testState.setGameStage(testState.getGameStage()+1) ;
         assertTrue(testState.getGameStage() == 14);
-
-
     }
 
     @Test
